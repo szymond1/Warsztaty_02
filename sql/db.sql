b84a0fb5e48a21cedc31604cc4bf6e3cbd4d8f74
dubiel.szymon@gmail.com <dubiel.szymon@gmail.com>
2017-11-17 15:18:32 +0100

    Add new tables to sql databases

---
+++ sql/db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE user_group (
+	id INT,
+	name VARCHAR(255),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE users(
+	id BIGINT(20),
+	username VARCHAR(255),
+	email VARCHAR(255) UNIQUE,
+	password VARCHAR(245),
+	user_group_id INT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(user_group_id) REFERENCES user_group(id)
+	);
