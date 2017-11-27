77cf72d2d452d3866da56ee0abdaa510a07a03ac
dubiel.szymon@gmail.com <dubiel.szymon@gmail.com>
2017-11-27 17:19:13 +0100

    dodano 2 klasy User i UserApp

--- src/Model/User.java
+++ src/Model/User.java
@@ -4,34 +4,36 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.mindrot.jbcrypt.BCrypt;
 
 public class User {
-	
+
 	private long id;
-	private String username; 
-	private String email; 
-	private String password; 
+	private String username;
+	private String email;
+	private String password;
 	private int userGroupId;
-	
+
 	public User() {
 		super();
-		this.id=0l;
+		this.id = 0l;
 		this.username = username;
 		this.email = email;
-		setPassword(password);
-		this.userGroupId=0;
+		this.setPassword(password);
+		this.userGroupId = 0;
 	}
-	
+
 	public User(String username, String email, String password) {
 		super();
-		this.id=0l;
+		this.id = 0l;
 		this.username = username;
 		this.email = email;
 		setPassword(password);
-		this.userGroupId=0;
+		this.userGroupId = 0;
 	}
 
 	public String getUsername() {
@@ -61,8 +63,7 @@ public class User {
 	public void checkPassword(String password) {
 		BCrypt.checkpw(password, this.password);
 	}
-	
-	
+
 	public int getUserGroupId() {
 		return userGroupId;
 	}
@@ -73,13 +74,13 @@ public class User {
 
 	public long getId() {
 		return id;
-	} 
-	
+	}
+
 	public void save(Connection conn) throws SQLException {
-		if(this.id == 0) {
+		if (this.id == 0) {
 			String sql = "INSERT INTO users(username, email, password, user_group_id) VALUES (?,?,?,?);";
-		
-			String[] generatedColumns = {"ID"}; // kolumna jest AUTOINCREMENT, wiec tworzymy tabele, która generuje ID
+
+			String[] generatedColumns = { "ID" }; // kolumna jest AUTOINCREMENT, wiec tworzymy tabele, która generuje ID
 			PreparedStatement ps = conn.prepareStatement(sql, generatedColumns);
 			ps.setString(1, this.username);
 			ps.setString(2, this.email);
@@ -88,14 +89,13 @@ public class User {
 			ps.executeUpdate();
 			ResultSet rs = ps.getGeneratedKeys(); // baza zwraca wynik
 			if (rs.next()) {
-				this.id = rs.getLong(1);	
+				this.id = rs.getLong(1);
 			}
 			ps.close();
 			rs.close();
-			
+
 		} else {
-			String sql = "UPDATE users SET username=?, email=?, password=?, user_group_id=? "
-					+ "WHERE id=?;";
+			String sql = "UPDATE users SET username=?, email=?, password=?, user_group_id=? " + "WHERE id=?;";
 			PreparedStatement ps1 = conn.prepareStatement(sql);
 			ps1.setString(1, this.username);
 			ps1.setString(2, this.email);
@@ -105,12 +105,50 @@ public class User {
 			ps1.executeUpdate();
 			ps1.close();
 		}
-	}	
-	
-	public static User getById(long id) {
-		String sql = "";
-		//execute sql
-		User u = new User();
-		return u;
 	}
+
+	public static User loadUserById(Connection conn, long id) throws SQLException {
+		String sql = "SELECT * FROM users WHERE id = ?";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setLong(1, id);
+		ResultSet rs = ps.executeQuery();
+		if (rs.next()) {
+			User loadedUser = new User();
+			loadedUser.id = rs.getLong("id");
+			loadedUser.username = rs.getString("username");
+			loadedUser.email = rs.getString("email");
+			loadedUser.password = rs.getString("password");
+			loadedUser.userGroupId = rs.getInt("user_group_id");
+			return loadedUser;
+		}
+		return null;
+	}
+
+	public static List<User> loadAllUser(Connection conn) throws SQLException {
+		String sql = "SELECT * FROM users";
+		List<User> allUserList = new ArrayList<>();
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			User loadUser = new User();
+			loadUser.id = rs.getLong("id");
+			loadUser.username = rs.getString("username");
+			loadUser.email = rs.getString("email");
+			loadUser.password = rs.getString("password");
+			loadUser.userGroupId = rs.getInt("user_group_id");
+			allUserList.add(loadUser);
+		}
+		return allUserList;
+	}
+
+	public void deleteUser(Connection conn, long id) throws SQLException {
+		if (this.id != 0) {
+			String sql = "DELETE FROM users WHERE id= ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setLong(1, this.id);
+			ps.executeUpdate();
+			this.id = 0;
+		}
+	}
+
 }
--- src/app/UserApp.java
+++ src/app/UserApp.java
@@ -12,10 +12,24 @@ public static void main(String[] args) {
 	
 	Connection conn=null;
 	try {
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/warsztat2?useSSL=false", "root", "coderslab");
-		User u =new User("szymond", "dubiel.szymon@gmail.com", "onett");
-		u.setUserGroupId(1);
-		u.save(conn);
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/warsztat2?useSSL=false", "root","coderslab");
+//		User u =new User();
+//		u.setUsername("Ola");
+//		u.setEmail("ola.stryczniewiczz@gmail.com");
+//		u.setPassword("gagagaga");
+//		u.setUserGroupId(1);
+//		u.save(conn);
+		User user1 = User.loadUserById(conn, 8);
+		System.out.println(user1.getUsername() + " " + user1.getEmail() + " " + user1.getPassword() );
+		
+		user1.setUsername("joannastr");
+		user1.setEmail("joannastr@gmail.com");
+		user1.setPassword("askakolaska");
+		
+		user1.save(conn);
+		
+		
+
 		conn.close();
 		// User u2 loadbyId
 	} catch (SQLException e) {
